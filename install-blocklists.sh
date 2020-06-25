0ed10f1223321b8b7fc308e7defc285d1949d158
arteeh <accounts@arteeh.com>
2020-06-25 10:54:40 +0200

    script

--- install-blocklists.sh
+++ install-blocklists.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# Update the repository
+git pull
 
+# Copy the list to the blocklist
+cp /etc/adsweep/blocklists/adlists.list /etc/pihole/adlists.list
+
+# Clear the regex list
+pihole --regex --nuke
+
+# For every line in the regexes file, add it to the regexes list
+while read LINE
+do
+	pihole --regex $LINE
+done < FILE
